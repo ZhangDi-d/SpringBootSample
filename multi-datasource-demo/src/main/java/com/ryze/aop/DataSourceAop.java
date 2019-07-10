2a4f69b6bddfd8819da415da2921be4c2db4eb91
ZhangDi-d <1772085788@qq.com>
2019-07-10 17:49:27 +0800

    20190710_02

--- multi-datasource-demo/src/main/java/com/ryze/aop/DataSourceAop.java
+++ multi-datasource-demo/src/main/java/com/ryze/aop/DataSourceAop.java
@@ -1,6 +1,8 @@
 package com.ryze.aop;
 
+import com.ryze.bean.DBContextHolder;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -11,13 +13,29 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataSourceAop {
     @Pointcut("!@annotation(com.ryze.annotation.Master) && (execution(* com.ryze..service..*.select*(..))" +
-            "||execution(* com.ryze..service..*.get*(..))execution(* com.ryze..service..*.find*(..)))")
+            "||execution(* com.ryze..service..*.get*(..))||execution(* com.ryze..service..*.find*(..)))")
     public void readPointcut() {
 
     }
 
-    @Pointcut()
+    @Pointcut("@annotation(com.ryze.annotation.Master) " +
+            "|| execution(* com.ryze..*.service..*.insert*(..)) " +
+            "|| execution(* com.ryze..*.service..*.add*(..)) " +
+            "|| execution(* com.ryze..*.service..*.update*(..)) " +
+            "|| execution(* com.ryze..*.service..*.edit*(..)) " +
+            "|| execution(* com.ryze..*.service..*.delete*(..)) " +
+            "|| execution(* com.ryze..*.service..*.remove*(..))")
     public void writePointcut() {
 
+    }
+
+    @Before("readPointcut()")
+    public void read() {
+        DBContextHolder.slave1();
+    }
+
+    @Before("writePointcut()")
+    public void write() {
+        DBContextHolder.master();
     }
 }
