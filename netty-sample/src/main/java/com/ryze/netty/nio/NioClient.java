e6a86d7c571cc3f25d3a875d7c465586998bd4c4
ZhangDi-d <1772085788@qq.com>
2019-07-23 15:56:34 +0800

    20190723004

--- netty-sample/src/main/java/com/ryze/netty/nio/NioClient.java
+++ netty-sample/src/main/java/com/ryze/netty/nio/NioClient.java
@@ -1,7 +1,49 @@
 package com.ryze.netty.nio;
 
+import io.netty.channel.ServerChannel;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * Created by xueLai on 2019/7/23.
  */
 public class NioClient {
+    public static void main(String[] args) throws IOException {
+        // 创建一个选择器来处理Channel
+        Selector selector = Selector.open();
+        // 打开套接字通道
+        SocketChannel channel = SocketChannel.open();
+        // 设置此通道为非阻塞模式
+        channel.configureBlocking(false);
+        // 绑定端口
+        channel.bind(new InetSocketAddress("127.0.0.1", 9090));
+        channel.register(selector, SelectionKey.OP_CONNECT);
+
+        while (true) {
+            //阻塞方法
+            selector.select();
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+            while (iterator.hasNext()) {
+
+                SelectionKey key = iterator.next();
+                iterator.remove();
+                if (key.isConnectable()) {
+                    SocketChannel sc = (SocketChannel)key.channel();
+                    channel.finishConnect();
+                    System.out.println("客户端连接服务器成功!");
+                    
+
+                } else if (key.isReadable()) {
+
+                }
+            }
+        }
+    }
 }
--- netty-sample/src/main/java/com/ryze/netty/nio/NioServer.java
+++ netty-sample/src/main/java/com/ryze/netty/nio/NioServer.java
@@ -1,16 +1,18 @@
 package com.ryze.netty.nio;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by xueLai on 2019/7/23.
  */
 public class NioServer {
-    private final static int PORT = 6789;
+    private final static int PORT = 9090;
 
     private static void start() throws IOException {
         // 创建一个选择器来处理Channel
@@ -24,7 +26,68 @@ public class NioServer {
         // 向给定的选择器注册此通道的接受连接事件
         socketChannel.register(selector, SelectionKey.OP_ACCEPT);
         System.out.println("NioServer 已经启动,监听端口:" + PORT);
-        
+
+        while (true) {
+            int select = selector.select();
+            System.out.println("start=========>select:" + select);
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                iterator.remove();// 删除处理过的key
+                if (key.isAcceptable()) {
+                    ServerSocketChannel channel = (ServerSocketChannel) key.channel();
+                    // 接受新的连接，并将它注册到选择器
+                    SocketChannel sc = channel.accept();
+                    sc.configureBlocking(false);
+                    sc.register(selector, SelectionKey.OP_READ);
+                    System.out.println("接受的连接来自: " + sc);
+                    byte[] bytes = ("欢迎来自" + InetAddress.getLocalHost().getHostName()).getBytes();
+                    ByteBuffer byteBuff = ByteBuffer.allocate(1024);
+                    byteBuff.put(bytes);
+
+                    byteBuff.flip();
+                    sc.write(byteBuff);
+
+                } else if (key.isReadable()) {
+                    SocketChannel sc = (SocketChannel) key.channel();
+                    ByteBuffer byteBuff = ByteBuffer.allocate(1024);
+                    // 读取客户端发送过来的消息
+                    while (true) {
+                        byteBuff.clear();
+                        int read = 0;
+                        try {
+                            read = sc.read(byteBuff);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        if (read == 0) {
+                            break;
+                        }
+                        if (read == -1) {
+                            // 将对端链路关闭
+                            key.cancel();
+                            sc.close();
+                            break;
+                        }
+
+                        String s = new String(byteBuff.array(), 0, read);
+                        System.out.println("客户端发送过来的数据 = " + s);
+                        byteBuff.clear();
+                        byteBuff.put(("Did you say '" + s + "'?").getBytes());
+
+                        byteBuff.flip();
+                        sc.write(byteBuff);
+                    }
+
+                } else if (key.isWritable()) {
+                    System.out.println("writable...");
+                }
+            }
+
+
+        }
     }
 
     public static void main(String[] args) {
