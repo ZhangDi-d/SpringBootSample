ec88f3199b9a1f9f7d63928ad395dc883f2eb897
ZhangDi-d <1772085788@qq.com>
2019-07-25 17:41:18 +0800

    20190725002

--- netty-sample/src/main/java/com/ryze/netty/netty/client/NettyClient.java
+++ netty-sample/src/main/java/com/ryze/netty/netty/client/NettyClient.java
@@ -4,4 +4,25 @@ package com.ryze.netty.netty.client;
  * Created by xueLai on 2019/7/25.
  */
 public class NettyClient {
+    // 客户端要连接的服务端主机名或ip
+    private final String host;
+
+    // 客户端要连接的服务端端口号
+    private final int port;
+
+    public NettyClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void connect() {
+
+    }
+
+    public static void main(String[] args) {
+        NettyClient nettyClient = new NettyClient("127.0.0.1", 9010);
+
+        nettyClient.connect();
+    }
+
 }
--- netty-sample/src/main/java/com/ryze/netty/netty/server/NettyServerHandler.java
+++ netty-sample/src/main/java/com/ryze/netty/netty/server/NettyServerHandler.java
@@ -1,9 +1,20 @@
 package com.ryze.netty.netty.server;
 
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 /**
  * Created by xueLai on 2019/7/25.
  */
 public class NettyServerHandler extends SimpleChannelInboundHandler<Object> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("客户端发送过来的数据 = " + msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+        ctx.close();
+    }
 }
