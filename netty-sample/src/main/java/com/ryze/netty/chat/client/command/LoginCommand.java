3e6adc3e6354ed222f43bbe9379d81a18002c660
ZhangDi-d <1772085788@qq.com>
2019-07-30 17:34:05 +0800

    20190730002

--- netty-sample/src/main/java/com/ryze/netty/chat/client/command/LoginCommand.java
+++ netty-sample/src/main/java/com/ryze/netty/chat/client/command/LoginCommand.java
@@ -1,6 +1,7 @@
 package com.ryze.netty.chat.client.command;
 
 import com.ryze.netty.chat.ChatInfo;
+import com.ryze.netty.chat.util.StringUtil;
 import io.netty.channel.Channel;
 
 /**
@@ -13,6 +14,21 @@ public class LoginCommand extends AbstractCommand {
 
     @Override
     public ChatInfo.Chat parseInput(String input) {
-        return null;
+        if (StringUtil.isBlank(input)) {
+            System.out.println("请输入用户名和密码");
+            return null;
+        }
+        String[] lineToUse = input.split(",");
+        if (lineToUse.length != 2) {
+            System.out.println("登录参数错误");
+            return null;
+        }
+        String username = lineToUse[0];
+        String password = lineToUse[1];
+        ChatInfo.User user = ChatInfo.User.newBuilder().setUsername(username).setPassword(password).build();
+        ChatInfo.LoginRequest loginRequest = ChatInfo.LoginRequest.newBuilder().setLoginUser(user).build();
+        ChatInfo.Chat chat = ChatInfo.Chat.newBuilder().setMsgType(ChatInfo.Chat.MessageType.LOGIN_REQUEST).setLoginRequest(loginRequest).build();
+        return chat;
+
     }
 }
