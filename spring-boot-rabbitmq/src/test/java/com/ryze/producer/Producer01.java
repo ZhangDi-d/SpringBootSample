10dfd82fe72b77b4e0a0bd68a7b76c7dd83ebc8b
ZhangDi-d <1772085788@qq.com>
2019-08-26 17:18:10 +0800

    20190826002

--- spring-boot-rabbitmq/src/test/java/com/ryze/producer/Producer01.java
+++ spring-boot-rabbitmq/src/test/java/com/ryze/producer/Producer01.java
@@ -1,5 +1,12 @@
 package com.ryze.producer;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 /**
  * Created by xueLai on 2019/8/26.
  */
@@ -7,6 +14,36 @@ public class Producer01 {
     final static String QUEUE = "helloworld";
 
     public static void main(String[] args) {
+        Connection connection = null;
+        Channel channel = null;
 
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("127.0.0.1");
+            factory.setPort(5762);
+            factory.setUsername("guest");
+            factory.setPassword("guest");
+            ////rabbitmq默认虚拟机名称为“/”，虚拟机相当于一个独立的mq服务器
+            factory.setVirtualHost("/");
+            //创建与RabbitMQ服务的TCP连接
+            connection = factory.newConnection();
+            //创建与Exchange的通道，每个连接可以创建多个通道，每个通道代表一个会话任务
+            channel = connection.createChannel();
+
+            /**
+             * 声明队列，如果Rabbit中没有此队列将自动创建
+             * param1:队列名称
+             * param2:是否持久化
+             * param3:队列是否独占此连接
+             * param4:队列不再使用时是否自动删除此队列
+             * param5:队列参数
+             */
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } finally {
+        }
     }
 }
