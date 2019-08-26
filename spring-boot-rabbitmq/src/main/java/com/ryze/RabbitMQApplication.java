4fdbf4acd961b6309629a3d148d60a42a6d57661
ZhangDi-d <1772085788@qq.com>
2019-08-26 16:08:36 +0800

    20190826001

---
+++ spring-boot-rabbitmq/src/main/java/com/ryze/RabbitMQApplication.java
@@ -0,0 +1,15 @@
+package com.ryze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by xueLai on 2019/8/23.
+ */
+@SpringBootApplication
+public class RabbitMQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMQApplication.class);
+    }
+}
---
+++ spring-boot-rabbitmq/src/test/java/com/ryze/producer/Producer01.java
@@ -0,0 +1,12 @@
+package com.ryze.producer;
+
+/**
+ * Created by xueLai on 2019/8/26.
+ */
+public class Producer01 {
+    final static String QUEUE = "helloworld";
+
+    public static void main(String[] args) {
+
+    }
+}
