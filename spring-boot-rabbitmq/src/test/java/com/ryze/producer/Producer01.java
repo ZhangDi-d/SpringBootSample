b202be896b841090cf592147d0767d49fe62c174
ZhangDi-d <1772085788@qq.com>
2019-08-27 13:49:51 +0800

    20190827001

--- spring-boot-rabbitmq/src/test/java/com/ryze/producer/Producer01.java
+++ spring-boot-rabbitmq/src/test/java/com/ryze/producer/Producer01.java
@@ -3,6 +3,8 @@ package com.ryze.producer;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -11,9 +13,11 @@ import java.util.concurrent.TimeoutException;
  * Created by xueLai on 2019/8/26.
  */
 public class Producer01 {
-    final static String QUEUE = "helloworld";
 
-    public static void main(String[] args) {
+    private final static Logger logger = LoggerFactory.getLogger(Producer01.class);
+    private final static String QUEUE = "helloworld";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
         Connection connection = null;
         Channel channel = null;
 
@@ -38,12 +42,35 @@ public class Producer01 {
              * param4:队列不再使用时是否自动删除此队列
              * param5:队列参数
              */
-            channel.queueDeclare();
+            channel.queueDeclare(QUEUE, true, false, false, null);
+            String message = "hello mq," + System.currentTimeMillis();
+
+            /**
+             * 消息发布方法
+             * param1：Exchange的名称，如果没有指定，则使用Default Exchange
+             * param2:routingKey,消息的路由Key，是用于Exchange（交换机）将消息转发到指定的消息队列
+             * param3:消息包含的属性
+             * param4：消息体
+             */
+            /**
+             * 这里没有指定交换机，消息将发送给默认交换机，每个队列也会绑定那个默认的交换机，但是不能显
+             示绑定或解除绑定
+             * 默认的交换机，routingKey等于队列名称
+             */
+
+            channel.basicPublish("", QUEUE, null, message.getBytes("utf-8"));
+            logger.info("send message=" + message);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (TimeoutException e) {
             e.printStackTrace();
         } finally {
+            if (channel != null) {
+                channel.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
         }
     }
 }
